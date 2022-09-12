e384af9c25e3d6f8b4b27a136f481e1e4f4c5b7d
Mauricio Fuentes <mauriciofb@gmail.com>
2022-09-12 11:12:10 -0300

    first

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root 'welcome#index'
+
+  get '/about', to: 'welcome#about', as: :about
+  get '/articles', to: 'articles#index'
+  get '/articles/new', to: 'articles#new', as: :new_article
+  get '/articles/:id', to: 'articles#show', as: :article
+  post '/articles', to: 'articles#create'
+  get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch '/articles/:id', to: 'articles#update'
+  delete '/articles/:id', to: 'articles#destroy'
+end
